bd81fc83c5528b0577e0e63f003fad5d6b775443
nathanveach <nathanbveach@gmail.com>
2021-02-16 16:39:43 -0800

    Created routes for controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
+  namespace :api do
+    namespace :v1 do
+      get 'authors/index'
+      get 'authors/:id', to: 'authors#show'
+      get 'books/index'
+      get 'books/:id', to: 'books#show'
+      get 'books/references', to: 'books#referenceList'
+      post 'duplicator/create/:id', to: 'duplicator#makeDuplicate'
+      patch 'duplicator/update/:id', to: 'duplicator#changeReference'
+      delete 'duplicator/destroy/:id', to: 'duplicator#makeReference'
+    end
+  end
+  
+  root 'homepage#index'
+  get '*path', to: 'homepage#index'
 end
